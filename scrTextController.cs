9e03eb0c44c23a40437729e27afed44b489a965e
FunkyFedora <jcward00@knights.ucf.edu>
2020-01-15 15:57:44 -0500

    Jenna Ward's Hello World

---
+++ scrTextController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scrTextController : MonoBehaviour
+{
+    public Text newText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            newText.text = "Hello, my name is Jenna Ward!";
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
